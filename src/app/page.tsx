5c01099c8a6c4cf113975da1f7a31241826ff926
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-08-21 15:42:51 +0300

    some changes 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By 2{" "}
+            By 3{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
